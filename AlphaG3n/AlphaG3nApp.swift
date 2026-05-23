b73223cc8eb801ee577687c4620471de54b30168
Peter <89460630+Peter-Zhao-751@users.noreply.github.com>
2026-05-23 12:00:49 -0700

    Initial Commit

---
+++ AlphaG3n/AlphaG3nApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlphaG3nApp.swift
+//  AlphaG3n
+//
+//  Created by Peter Zhao on 5/23/26.
+//
+
+import SwiftUI
+
+@main
+struct AlphaG3nApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
